813bedcbf76e01126cd52f3866c6c37c0310b3cc
iitimii <timiiowolabi@gmail.com>
2024-06-13 18:05:31 +0100

    update

--- register_new_face.py
+++ register_new_face.py
@@ -1,23 +1,27 @@
 import os
 import cv2
 from train_model import train_model
+from picamera2 import Picamera2
 
-name = 'Kenny' 
+name = 'Victory' 
 output_directory = f"dataset/{name}" 
 os.makedirs(output_directory, exist_ok=True)
 
-cam = cv2.VideoCapture(0)
 
 cv2.namedWindow("press space to take a photo", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("press space to take a photo", 500, 300)
 
 img_counter = 0
 
+picam2 = Picamera2()
+picam2.preview_configuration.main.size = (640, 640)
+picam2.preview_configuration.main.format = "RGB888"
+picam2.preview_configuration.align()
+picam2.configure("preview")
+picam2.start()
+
 while True:
-    ret, frame = cam.read()
-    if not ret:
-        print("failed to grab frame")
-        break
+    frame = picam2.capture_array()
     cv2.imshow("press space to take a photo", frame)
 
     k = cv2.waitKey(1)
@@ -32,7 +36,6 @@ while True:
         print("{} written!".format(img_name))
         img_counter += 1
 
-cam.release()
 cv2.destroyAllWindows()
 
 train_model()
